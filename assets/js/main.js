d14b9488e2a855e33407aa8f72cadf68696bbe0e
ccthecode <ccthecode@gmail.com>
2024-07-17 23:25:27 +0100

    fade "open for business" animation on scroll down

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,23 @@ menuClose.addEventListener('click', function(){
 menuBtn.addEventListener('click', function(){
   menuMobile.classList.toggle('active')
 })
+
+
+// open for business
+const circleWrapper = document.querySelector('#circle-wrapper');
+
+window.addEventListener("DOMContentLoaded", function(){
+  circleWrapper.classList.add('active');
+})
+
+window.addEventListener('scroll', function() {
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+  
+  if (scrollTop > 700) {
+    circleWrapper.classList.remove('active');
+  }
+
+  if (scrollTop <= 10) {
+    circleWrapper.classList.add('active');
+  }
+});
